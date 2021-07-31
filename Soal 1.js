51d6c4a3f84fd68322b999563a2b0cbde76c8be8
DiazRizky <86656562+DiazRizky@users.noreply.github.com>
2021-07-31 10:43:32 +0700

    Add files via upload

---
+++ Soal 1.js
@@ -0,0 +1,6 @@
+var nilaiSantri = [ 52, 95, 55, 67, 79, 44, 30, 97, 84 ];
+ var total = 0;
+ for(var i = 0; i<nilaiSantri.length; i++){
+     total +=nilaiSantri[i]
+ }
+ console.log(total/nilaiSantri.length); 
